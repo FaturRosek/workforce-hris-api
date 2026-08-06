6a6ef66d46109bda0de67a6aa0a08c597c422b7f
FaturRosek <faturrosek0403@gmail.com>
2026-08-06 16:02:01 +0700

    Feat: add position

---
+++ src/database/seeders/002_seed_positions.sql
@@ -0,0 +1,21 @@
+INSERT INTO positions (department_id, position_name, base_salary)
+VALUES
+(1, 'Backend Developer', 7000000),
+(1, 'Frontend Developer', 7000000),
+(1, 'QA Engineer', 6500000),
+
+(2, 'HR Officer', 6000000),
+(2, 'Recruiter', 6000000),
+
+(3, 'Finance Staff', 6500000),
+
+(4, 'Production Operator', 5000000),
+(4, 'Production Supervisor', 8000000),
+
+(5, 'QC Inspector', 5500000),
+
+(6, 'Warehouse Staff', 5000000),
+
+(7, 'Purchasing Staff', 6000000),
+
+(8, 'Marketing Staff', 6000000);
