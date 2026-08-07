cbb8038f20716dd43c6e3a06cf9acc6b3480294a
FaturRosek <faturrosek0403@gmail.com>
2026-08-07 15:59:29 +0700

    Feat: connect server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const app = require("./app");
+const pool = require("./config/db");
+
+const PORT = process.env.PORT || 3000;
+
+pool
+  .connect()
+  .then(() => {
+    console.log("PostgreSQL Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
